72a58ae9fd65054ab012427d3da82b26170b12dc
Timothy Ha <timothy@chen-ha.com>
2020-05-28 08:54:36 -0700

    cleanup comments

--- badges.cpp
+++ badges.cpp
@@ -38,61 +38,7 @@ int main() {
 	read("badges/ethics", ethics);
 	read("badges/leadership-community-and-engaging-world", leadership);
 
-	/* cout << "Badges:\n";
-	cout << "01. Media and Visual Analysis\n";
-	cout << "02. Scientific Method\n";
-	cout << "03. Literary and Textual Analysis\n";
-	cout << "04. Quantitative and Numerical Analysis\n";
-	cout << "05. Societies and Cultures of the Past\n";
-	cout << "06. Diversity & Identity\n";
-	cout << "07. Global Awareness\n";
-	cout << "08. Sustainability\n";
-	cout << "09. Practical and Applied Knowledge\n";
-	cout << "10. Ethics\n";
-	cout << "11. Leadership, Community, and Engaging the World\n";
-
-	cin >> input;
-
-	switch(input) {
-		case 1:
-			for (int i = 0; i < media.size(); i++) cout << media.at(i) << "\n";
-			break;
-		case 2:
-			for (int i = 0; i < sciMethod.size(); i++) cout << sciMethod.at(i) << "\n";
-			break;
-		case 3:
-			for (int i = 0; i < literary.size(); i++) cout << literary.at(i) << "\n";
-			break;
-		case 4:
-			for (int i = 0; i < quant.size(); i++) cout << quant.at(i) << "\n";
-			break;
-		case 5:
-			for (int i = 0; i < societies.size(); i++) cout << societies.at(i) << "\n";
-			break;
-		case 6:
-			for (int i = 0; i < diversity.size(); i++) cout << diversity.at(i) << "\n";
-			break;
-		case 7:
-			for (int i = 0; i < awareness.size(); i++) cout << awareness.at(i) << "\n";
-			break;
-		case 8:
-			for (int i = 0; i < sustainability.size(); i++) cout << sustainability.at(i) << "\n";
-			break;
-		case 9:
-			for (int i = 0; i < appKnowledge.size(); i++) cout << appKnowledge.at(i) << "\n";
-			break;
-		case 10:
-			for (int i = 0; i < ethics.size(); i++) cout << ethics.at(i) << "\n";
-			break;
-		case 11:
-			for (int i = 0; i < leadership.size(); i++) cout << leadership.at(i) << "\n";
-			break;
-		default:
-			cout << "Invalid input.";
-	} */
-	string course;
-
-	
+	string course;	
 	cout << "Usage: deptName XXX\n";
 	cout << "Example: CSE 021\n";
 
